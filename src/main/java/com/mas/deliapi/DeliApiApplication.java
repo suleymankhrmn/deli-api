f180117514dd7f0a293d3f92b33177b7b8e28e41
suleymankhrmn <skahraman1834@gmail.com>
2020-10-02 11:10:05 +0300

    first commit

---
+++ src/main/java/com/mas/deliapi/DeliApiApplication.java
@@ -0,0 +1,13 @@
+package com.mas.deliapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mas/deliapi/DeliApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mas.deliapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
