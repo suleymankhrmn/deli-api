3a572e05f6aacedfbd3efd053a64a218aaeb71b3
makifoz <ozdemir.mhmtakif@gmail.com>
2020-12-02 20:58:43 +0300

    add tansaction #1

--- src/main/java/com/mas/deliapi/service/CommonServiceImpl.java
+++ src/main/java/com/mas/deliapi/service/CommonServiceImpl.java
@@ -4,6 +4,7 @@ import com.mas.deliapi.dao.CommonDAO;
 import com.mas.deliapi.entity.BaseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 
@@ -18,26 +19,31 @@ public  class CommonServiceImpl<T extends BaseEntity> implements CommonService<T
     }
 
     @Override
+    @Transactional
     public T getById(long id) {
         return commonDAO.getById(id);
     }
 
     @Override
+    @Transactional
     public List<T> getList() {
         return commonDAO.getList();
     }
 
     @Override
+    @Transactional
     public List<T> getActives() {
         return commonDAO.getActives();
     }
 
     @Override
+    @Transactional
     public void saveOrUpdate(T t) {
         commonDAO.saveOrUpdate(t);
     }
 
     @Override
+    @Transactional
     public void delete(long id) {
         T item = commonDAO.getById(id);
         item.setActive(false);
